bfa7b74d7b5e431ac3fe6e08be311903dc963f1a
Gitchannnz <20201078@nbsc.edu.ph>
2025-05-25 16:03:50 +0800

    fix again

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import { Routes, Route } from "react-router-dom";
-import Header from "./pages/Header/Header";
-import Home from "./pages/home/Home";
+import Header from "./pages/Header/Header_main";
+import Home from "./pages/Home/Home_main";
 import Aboutme from "./pages/aboutme/Aboutme";
 import Skill from "./pages/Skill/Skill_main";
 import Projects from "./pages/Projects/Projects_main";
